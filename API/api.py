7591749aef40088db4360be0d98b136855444837
unknown <aude9500@gmail.com>
2023-10-26 15:28:31 +0900

    tet

---
+++ API/api.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import psycopg2
+import requests
+import urllib
+from urllib.parse import urlencode, unquote, quote_plus
+from urllib.request import urlopen
+
+from DB import db as db
+
+
+
+
+
+if __name__ == '__main__':
